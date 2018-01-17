07c7beed392f6c3bc1616af5e2be73f5293beb97
Danielle Beyer <dwishnuff@gmail.com>
2018-01-16 17:27:48 -0800

    updated with videos

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,5 @@
 //array of video source files to loop through
-var videoSources = ["public/media/SmartCourageous.mp4","public/media/Hilton.mp4", "public/media/Batman.mp4"];
+var videoSources = ["public/media/funny.MOV","public/media/SmartCourageous.mp4","public/media/silly2.mp4","public/media/thebest.MOV","public/media/smart.mp4", "public/media/fun.mp4", "public/media/silly.MOV"];
 
 var currentIndex = 0;
 // listener function changes src
@@ -13,9 +13,9 @@ function myNewSrc() {
 // add a listener function to the ended event
 function myAddListener(){
   var myVideo = document.getElementsByTagName('video')[0];
-  currentIndex = (currentIndex+1) % videoSources.length;
+  currentIndex = (currentIndex+1);
   myVideo.src = videoSources[currentIndex];
-  myVideo.addEventListener('ended', myNewSrc, false);
+  // myVideo.addEventListener('ended', myNewSrc, false);
 
 }
 
